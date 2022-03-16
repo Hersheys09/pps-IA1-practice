f184835ca923fffaeeb7ed7fea47c2de296f6214
Harshit Singh <89565061+harshitsingh09@users.noreply.github.com>
2022-03-16 04:32:56 +0000

    p4 done with debugging

---
+++ p4final.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int input_array_size()
+{
+  int x;
+  printf("Enter the no. of values you will enter:\n");
+  scanf("%d", &x);
+  return x;
+}
+
+void input_array(int n, int a[n])
+{
+  for(int i=0; n>i; i++)
+  {
+    printf("Enter a number: ");
+    scanf("%d", &a[i]);
+  }
+}
+
+int sum_n_arrays(int n, int a[n])
+{
+  int sum = 0;
+  for(int i=0;i<n; i++)
+  {
+    sum = sum + a[i];
+  }
+  return sum;
+}
+
+void out_put(int n, int a[n], int sum)
+{
+  for(int i=0;i<n-1; i++)
+  {
+    printf("%d + ", a[i]);
+  }
+  printf("%d = %d", a[n-1], sum);
+}
+
+int main()
+{
+  int n, sum;
+  n = input_array_size();
+  int a[n];
+  input_array(n, a);
+  sum = sum_n_arrays(n, a);
+  out_put(n, a, sum);
+  return 0;
+}
